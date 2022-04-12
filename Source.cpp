05aff8dc1b32a65a58e42fa4308d042c88d58977
Taufiq <61982529+Uplus1D70F@users.noreply.github.com>
2022-04-12 14:02:40 +0400

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+// C++ program to print all primes
+// less than N
+#include <iostream>
+using namespace std;
+
+// function check whether a number
+// is prime or not
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+
+    // Check from 2 to n-1
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+// Function to print primes
+void printPrime(int n)
+{
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i))
+            cout << i << " ";
+    }
+}
+
+int main()
+{
+    int n = 7;
+    printPrime(n);
+}
